dee41872b56c39d49a097e0f74e28ba85cc7ee9d
SmaranTum <98882002+SmaranTum@users.noreply.github.com>
2022-02-17 14:38:40 +0100

    Add files via upload

---
+++ tools/syrec_editor.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+import os, re, sys
+
+from PyQt4 import * #QtCore, QtGui, Qsci
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+#from PyQt4.Qsci import *
+
+from revkit import *
+from revkitui import *
+
+
+
+class LogWidget( QTreeWidget ):
+    def __init__( self, parent = None ):
+        QTreeWidget.__init__( self, parent )
+
+        self.setRootIsDecorated( False )
+        self.setHeaderLabels( [ "Line", "Message" ] )
+
+    def addMessage( self, line, message ):
+        item = QTreeWidgetItem( [ str( line ), message ] )
+        self.addTopLevelItem( item )
+
+class MainWindow( QMainWindow ):
+    def __init__( self, parent = None ):
+        QWidget.__init__( self, parent )
+
+        self.setWindowTitle( "Text Editor" )
+
+        self.setupWidgets()
+        self.setupDockWidgets()
+        self.setupActions()
+        self.setupToolBar()
+
+    def setupWidgets( self ):
+        self.editor = QtSyReCEditor( self )
+        self.viewer = CircuitView( None, self )
+
+        splitter = QSplitter( Qt.Vertical, self )
+        splitter.addWidget( self.editor.widget )
+        splitter.addWidget( self.viewer )
+
+        self.setCentralWidget( splitter )
+
+    def setupDockWidgets( self ):
+        self.logWidget = LogWidget( self )
+        self.logDockWidget = QDockWidget( "Log Messages", self )
+        self.logDockWidget.setWidget( self.logWidget )
+        self.addDockWidget( Qt.BottomDockWidgetArea, self.logDockWidget )
+
+    def setupActions( self ):
+        self.editor.before_build = self.logWidget.clear
+        self.editor.build_successful = lambda circ: self.viewer.load( circ )
+        self.editor.build_failed = lambda error_message: self.logWidget.addMessage( re.search ( 'In line (.*): (.*)', error_message ).group( 1 ), \
+                                                                                    re.search ( 'In line (.*): (.*)', error_message ).group( 2 ) )
+
+    def setupToolBar( self ):
+        toolbar = self.addToolBar( 'Main' )
+        toolbar.setIconSize( QSize( 32, 32 ) )
+
+        toolbar.addAction( self.editor.openAction )
+        toolbar.addAction( self.editor.buildAction )
+        toolbar.addAction( self.editor.simAction )
+        toolbar.addAction( self.editor.statAction )
+
+if __name__ == "__main__":
+    a = QApplication([])
+
+    w = MainWindow()
+    w.show()
+
+    sys.exit( a.exec_() )
