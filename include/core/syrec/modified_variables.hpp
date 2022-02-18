e04d089b22aacfad70a60345d78106889d2a548a
SmaranTum <98882002+SmaranTum@users.noreply.github.com>
2022-02-19 00:28:08 +0100

    Add files via upload

---
+++ include/core/syrec/modified_variables.hpp
@@ -0,0 +1,20 @@
+/**
+ * @brief TODO
+ *
+ * @file modified_variables.hpp
+ */
+
+#include <core/syrec/program.hpp>
+#include <core/syrec/statement.hpp>
+#include <core/syrec/variable.hpp>
+#include <map>
+#include <set>
+
+namespace revkit {
+    namespace syrec {
+
+        void modified_variables(const program& prog,
+                                std::map<statement::ptr, std::set<variable::ptr>>&);
+
+    }
+} // namespace revkit
---
+++ include/core/syrec/reverse_statements.hpp
@@ -0,0 +1,19 @@
+/**
+ * @brief Reverse Statements
+ *
+ * @file reverse_statements.hpp
+ */
+
+#include <core/syrec/statement.hpp>
+
+namespace revkit {
+    namespace syrec {
+
+        struct reverse_statements {
+            typedef statement::ptr result_type;
+
+            statement::ptr operator()(statement::ptr _statement) const;
+        };
+
+    } // namespace syrec
+} // namespace revkit
