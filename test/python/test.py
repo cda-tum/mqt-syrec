c7600da158b6cadbb640a9e233d98e096c53a254
SmaranTum <smaran.adarsh@tum.de>
2022-03-04 14:11:00 +0100

    coverage

--- test/python/test.py
+++ test/python/test.py
@@ -1,7 +1,5 @@
 from mqt import syrec
 
 if __name__ == "__main__":
-    dummy = syrec.Dummy()
-    dummy.setVal(42)
-    print(dummy.getVal())
-    dummy = syrec.Dummy(42)
+    circ = syrec.circuit()
+
