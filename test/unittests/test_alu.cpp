befcc0b073914d369844a010f733547a600d92cb
SmaranTum <smaran.adarsh@tum.de>
2022-03-08 19:35:05 +0100

    testing

--- test/unittests/test_alu.cpp
+++ test/unittests/test_alu.cpp
@@ -3,11 +3,15 @@
 #include "core/syrec/program.hpp"
 #include "core/test_functions.hpp"
 #include "gtest/gtest.h"
-#include <algorithms/synthesis/syrec_synthesis.hpp>
+#include "algorithms/synthesis/syrec_synthesis.hpp"
+#include "core/properties.hpp"
+#include "core/utils/costs.hpp"
+
 #include <boost/dynamic_bitset.hpp>
-#include <core/properties.hpp>
-#include <core/utils/costs.hpp>
+#include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
 
 namespace syrec {
     class syrec_test_alu: public ::testing::Test {
@@ -24,7 +28,21 @@ namespace syrec {
         boost::dynamic_bitset<> input;
         boost::dynamic_bitset<> output;
 
-        void SetUp() override {
+    unsigned expected_num_gates;
+    unsigned expected_lines;
+    cost_t expected_qc;
+    cost_t expected_tc;
+    std::string expected_sim_out;
+
+    std::vector<std::vector<std::string>> content;
+    std::vector<std::string> row;
+    std::vector<int> set_lines;
+    std::string line, word;
+    std::fstream file;
+    std::string file_name;
+    std::string output_string;
+
+    void SetUp() override {
             // setup all the individual objects before each test
 
             file.open("test.csv", std::ios::in);
@@ -45,22 +63,44 @@ namespace syrec {
     };
 
     TEST_F(syrec_test_alu, GenericTest_alu1) {
-        EXPECT_EQ(46, circ.num_gates());
+
+        for (int i = 1; i < (int) content.size(); i++) {
+
+            file_name.append("./circuits/");
+            file_name.append(content[i][0]);
+            file_name.append(".src");
+
+            expected_num_gates = std::stoi(content[i][1]);
+            expected_lines = std::stoi(content[i][2]);
+            expected_qc  =  (cost_t) std::stoi(content[i][3]);
+            expected_tc  =  (cost_t) std::stoi(content[i][4]);
+
+            std::stringstream str(content[i][5]);
+            while(getline(str, word, '-'))
+                set_lines.push_back(std::stoi(word));
+
+            expected_sim_out = content[i][6];
+
+            error_string = my_read_program(prog, file_name);
+            EXPECT_TRUE(error_string.empty());
+            EXPECT_TRUE(syrec::syrec_synthesis(circ, prog));
+            qc = syrec::final_quantum_cost(circ, circ.lines());
+            tc = syrec::final_transistor_cost(circ, circ.lines());
+            input.resize(circ.lines());
+
+            for(int line: set_lines)
+                input.set(line);
+
+            output.resize(circ.lines());
+            EXPECT_TRUE(simple_simulation(output, circ, input, settings, statistics));
+            boost::to_string(output, output_string);
+        }
+
+        EXPECT_EQ(expected_num_gates, circ.num_gates());
+        EXPECT_EQ(expected_lines, circ.lines());
+        EXPECT_EQ(expected_qc, qc);
+        EXPECT_EQ(expected_tc, tc);
+        EXPECT_EQ(expected_sim_out, output_string);
     }
 
-    TEST_F(syrec_test_alu, GenericTest_alu2) {
-        EXPECT_EQ(7, circ.lines());
-    }
-
-    TEST_F(syrec_test_alu, GenericTest_alu3) {
-        EXPECT_EQ(222, qc);
-    }
-
-    TEST_F(syrec_test_alu, GenericTest_alu4) {
-        EXPECT_EQ(640, tc);
-    }
-
-    TEST_F(syrec_test_alu, GenericTest_alu5) {
-        EXPECT_EQ("1010001", bitset_to_string(output));
-    }
 } // namespace syrec
