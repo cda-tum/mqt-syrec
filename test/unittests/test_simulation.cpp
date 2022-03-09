f61a2356560c24505aae29cccb357cb9929f2ab2
SmaranTum <smaran.adarsh@tum.de>
2022-03-09 19:49:13 +0100

    Updating google tests

--- test/unittests/test_simulation.cpp
+++ test/unittests/test_simulation.cpp
@@ -31,8 +31,7 @@ protected:
         std::string synthesis_param = GetParam();
         file_name                   = test_circuits_dir + GetParam() + ".src";
         std::ifstream i(test_circuits_dir + "circuits_simulation.json");
-        json          j;
-        i >> j;
+        json          j  = json::parse(i);
         expected_sim_out = j[synthesis_param]["sim_out"];
         set_lines        = j[synthesis_param]["set_lines"].get<std::vector<int>>();
     }
@@ -42,7 +41,7 @@ INSTANTIATE_TEST_SUITE_P(SyrecSimulationTest, SyrecSimulationTest,
                          testing::Values(
                                  "alu_2",
                                  "swap_2",
-                                 "shift_4"),
+                                 "simple_add_2"),
                          [](const testing::TestParamInfo<SyrecSimulationTest::ParamType>& info) {
                              auto s = info.param;
                              std::replace( s.begin(), s.end(), '-', '_');
