84be8df4380bfe40b79b2aec44b0f63b8e5a78a4
SmaranTum <smaran.adarsh@tum.de>
2022-07-25 09:44:07 +0200

    Minor changes to dd_synthesis.cpp and added additional tests.

--- src/core/truthTable/dd_synthesis.cpp
+++ src/core/truthTable/dd_synthesis.cpp
@@ -84,6 +84,7 @@ namespace syrec {
     }
 
     void algoP4(dd::mEdge& src, dd::mNode* current, syrec::TruthTable::Cube::Vector& p1SigVec, syrec::TruthTable::Cube::Vector& p2SigVec, const std::vector<std::size_t>& indices, std::unique_ptr<dd::Package<>>& dd) {
+        std::cout << "inside algoP4" << std::endl;
         syrec::TruthTable::Cube::Vector p2SigVecCpy = p2SigVec;
 
         syrec::TruthTable::Cube::Vector rootSigVec;
@@ -97,13 +98,16 @@ namespace syrec {
             for (std::size_t j = 0; j < i.size(); j++) {
                 if (i[j].has_value()) {
                     if (*(i[j])) {
+                        std::cout << "root" << std::endl;
+                        std::cout << "pos" << std::endl;
                         dd::Control obj;
                         obj.qubit = static_cast<dd::Qubit>((i.size() + current->v) - j);
                         obj.type  = dd::Control::Type::pos;
                         Ctrlroot.insert(obj);
                     } else {
+                        std::cout << "root" << std::endl;
+                        std::cout << "neg" << std::endl;
                         dd::Control obj;
-
                         obj.qubit = static_cast<dd::Qubit>((i.size() + current->v) - j);
                         obj.type  = dd::Control::Type::neg;
                         Ctrlroot.insert(obj);
@@ -112,59 +116,83 @@ namespace syrec {
             }
         }
 
-        dd::Control obj;
-        obj.qubit = current->v;
-        obj.type  = dd::Control::Type::pos;
-        Ctrlroot.insert(obj);
+        dd::Control currentNode;
+        currentNode.qubit = current->v;
+        currentNode.type  = dd::Control::Type::pos;
 
         for (auto ind: indices) {
             for (std::size_t p2Obj = 0; p2Obj < p2SigVecCpy[ind].size(); p2Obj++) {
                 dd::Controls Ctrl;
-                std::cout << p2Obj << std::endl;
                 if (*(p2SigVecCpy[ind][p2Obj])) {
+                    std::cout << "True -> False" << std::endl;
                     p2SigVecCpy[ind].at(p2Obj) = false;
                 } else {
+                    std::cout << "False -> True" << std::endl;
                     p2SigVecCpy[ind].at(p2Obj) = true;
                 }
 
                 auto it = std::find(p1SigVec.begin(), p1SigVec.end(), p2SigVecCpy[ind]);
                 if (it != p1SigVec.end()) {
+                    std::cout << "not changed to unique signature" << std::endl;
                     p2SigVecCpy = p2SigVec;
                     continue;
                 } else {
+                    std::cout << "changed to unique signature" << std::endl;
                     for (std::size_t p2ObjCpy = 0; p2ObjCpy < p2SigVec[ind].size(); p2ObjCpy++) {
                         if (p2ObjCpy != p2Obj) {
                             if (*(p2SigVec[ind][p2ObjCpy])) {
+                                std::cout << "not root" << std::endl;
+                                std::cout << "pos" << std::endl;
                                 dd::Control objP;
-                                obj.qubit = static_cast<dd::Qubit>(current->v - (p2ObjCpy + 1));
-                                obj.type  = dd::Control::Type::pos;
+                                objP.qubit = static_cast<dd::Qubit>(current->v - (p2ObjCpy + 1));
+                                objP.type  = dd::Control::Type::pos;
                                 Ctrl.insert(objP);
                             } else {
+                                std::cout << "not root" << std::endl;
+                                std::cout << "neg" << std::endl;
                                 dd::Control objN;
-                                obj.qubit = static_cast<dd::Qubit>(current->v - (p2ObjCpy + 1));
-                                obj.type  = dd::Control::Type::neg;
+                                objN.qubit = static_cast<dd::Qubit>(current->v - (p2ObjCpy + 1));
+                                objN.type  = dd::Control::Type::neg;
                                 Ctrl.insert(objN);
                             }
                         }
                     }
 
-                    Ctrl.insert(Ctrlroot.begin(), Ctrlroot.end());
+                    if (!Ctrlroot.empty()) {
+                        for (auto i: Ctrlroot) {
+                            dd::Controls ctrlFinal;
+                            ctrlFinal.insert(i);
+                            ctrlFinal.insert(currentNode);
+                            ctrlFinal.insert(Ctrl.begin(), Ctrl.end());
 
-                    auto q = qc::QuantumComputation(dd->qubits());
-                    q.x(static_cast<dd::Qubit>((std::size_t)current->v - (p2Obj + 1U)), Ctrl);
-                    Ctrl.clear();
-                    auto ddTest = dd::buildFunctionality(&q, dd);
-                    src         = dd->multiply(src, ddTest);
+                            auto q = qc::QuantumComputation(dd->qubits());
+                            q.x(static_cast<dd::Qubit>((std::size_t)current->v - (p2Obj + 1U)), ctrlFinal);
+                            ctrlFinal.clear();
+                            auto ddTest = dd::buildFunctionality(&q, dd);
+                            src         = dd->multiply(src, ddTest);
+                        }
+                    } else {
+                        dd::Controls ctrlFinal;
+                        ctrlFinal.insert(currentNode);
+                        ctrlFinal.insert(Ctrl.begin(), Ctrl.end());
+
+                        auto q = qc::QuantumComputation(dd->qubits());
+                        q.x(static_cast<dd::Qubit>((std::size_t)current->v - (p2Obj + 1U)), ctrlFinal);
+                        ctrlFinal.clear();
+                        auto ddTest = dd::buildFunctionality(&q, dd);
+                        src         = dd->multiply(src, ddTest);
+                    }
 
                     break;
                 }
             }
 
-            algoP2(src, src.p, dd);
+            algoQ(src, dd);
         }
     }
 
     bool algoP3(dd::mNode* current) {
+        std::cout << "inside algoP3" << std::endl;
         if (!(dd::mNode::isTerminal(current))) {
             if (!(current->e[0].isZeroTerminal()) && current->e[1].isZeroTerminal())
                 return true;
@@ -176,6 +204,7 @@ namespace syrec {
     }
 
     void algoP1(dd::mEdge& src, dd::mNode* current, std::unique_ptr<dd::Package<>>& dd) {
+        std::cout << "inside algoP1" << std::endl;
         if (!(dd::mNode::isTerminal(current))) {
             syrec::TruthTable::Cube::Vector p1SigVec;
             syrec::TruthTable::Cube         p1TempVec;
@@ -197,11 +226,15 @@ namespace syrec {
                 for (auto const& i: rootSigVec) {
                     for (std::size_t j = 0; j < i.size(); j++) {
                         if (*(i[j])) {
+                            std::cout << "root" << std::endl;
+                            std::cout << "pos" << std::endl;
                             dd::Control obj;
                             obj.qubit = static_cast<dd::Qubit>((i.size() + current->v) - j);
                             obj.type  = dd::Control::Type::pos;
                             Ctrl.insert(obj);
                         } else {
+                            std::cout << "root" << std::endl;
+                            std::cout << "neg" << std::endl;
                             dd::Control obj;
                             obj.qubit = static_cast<dd::Qubit>((i.size() + current->v) - j);
                             obj.type  = dd::Control::Type::neg;
@@ -220,6 +253,7 @@ namespace syrec {
     }
 
     void algoP2(dd::mEdge& src, dd::mNode* current, std::unique_ptr<dd::Package<>>& dd) {
+        std::cout << "inside algoP2" << std::endl;
         if (!(dd::mNode::isTerminal(current))) {
             syrec::TruthTable::Cube::Vector p1SigVec;
             syrec::TruthTable::Cube         p1TempVec;
@@ -252,31 +286,36 @@ namespace syrec {
 
                 for (auto const& i: rootSigVec) {
                     for (std::size_t j = 0; j < i.size(); j++) {
-                        if (i[j].has_value()) {
-                            if (*(i[j])) {
-                                dd::Control obj;
-                                obj.qubit = static_cast<dd::Qubit>((i.size() + current->v) - j);
-                                obj.type  = dd::Control::Type::pos;
-                                Ctrl.insert(obj);
-                            } else {
-                                dd::Control obj;
-                                obj.qubit = static_cast<dd::Qubit>((i.size() + current->v) - j);
-                                obj.type  = dd::Control::Type::neg;
-                                Ctrl.insert(obj);
-                            }
+                        if (*(i[j])) {
+                            std::cout << "root" << std::endl;
+                            std::cout << "pos" << std::endl;
+                            dd::Control obj;
+                            obj.qubit = static_cast<dd::Qubit>((i.size() + current->v) - j);
+                            obj.type  = dd::Control::Type::pos;
+                            Ctrl.insert(obj);
+                        } else {
+                            std::cout << "root" << std::endl;
+                            std::cout << "neg" << std::endl;
+                            dd::Control obj;
+                            obj.qubit = static_cast<dd::Qubit>((i.size() + current->v) - j);
+                            obj.type  = dd::Control::Type::neg;
+                            Ctrl.insert(obj);
                         }
                     }
 
                     for (auto const& p2: p2SigVec) {
                         for (std::size_t pj = 0; pj < p2.size(); pj++) {
                             if (*(p2[pj])) {
+                                std::cout << "not root" << std::endl;
+                                std::cout << "pos" << std::endl;
                                 dd::Control obj;
                                 obj.qubit = static_cast<dd::Qubit>(current->v - (pj + 1));
                                 obj.type  = dd::Control::Type::pos;
                                 Ctrl.insert(obj);
                             } else {
+                                std::cout << "not root" << std::endl;
+                                std::cout << "neg" << std::endl;
                                 dd::Control obj;
-
                                 obj.qubit = static_cast<dd::Qubit>(current->v - (pj + 1));
                                 obj.type  = dd::Control::Type::neg;
                                 Ctrl.insert(obj);
@@ -300,6 +339,7 @@ namespace syrec {
     }
 
     void algoP(dd::mEdge& src, dd::mNode* current, std::unique_ptr<dd::Package<>>& dd) {
+        std::cout << "inside algoP" << std::endl;
         auto srcCpy = src;
 
         if (algoP3(current)) {
@@ -314,6 +354,7 @@ namespace syrec {
     }
 
     void algoQ(dd::mEdge& src, std::unique_ptr<dd::Package<>>& dd) {
+        std::cout << "inside algoQ" << std::endl;
         std::vector<dd::mNode*> q;
         std::vector<dd::mNode*> visited;
 
@@ -322,6 +363,7 @@ namespace syrec {
         auto currNode = src.p;
 
         while (src != dd->makeIdent(static_cast<dd::Qubit>(dd->qubits()))) {
+            std::cout << "while" << std::endl;
             for (const auto& edge: currNode->e) {
                 if (!edge.isTerminal()) {
                     auto it = std::find(visited.begin(), visited.end(), edge.p);
