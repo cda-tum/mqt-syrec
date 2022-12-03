4d20966d957687f8f9c2fa3f3d21a534278381db
SmaranTum <smaran.adarsh@tum.de>
2022-12-04 00:19:50 +0100

    Modified completeCubes function from recursive to iterative.

--- src/core/truthTable/truth_table.cpp
+++ src/core/truthTable/truth_table.cpp
@@ -3,38 +3,28 @@
 namespace syrec {
 
     auto TruthTable::Cube::completeCubes() const -> Vector {
-        Vector result{};
-
-        // iterate over the values of the cube
-        for (std::size_t pos = 0U; pos < size(); ++pos) {
-            // skip any value that is not a don't care
-            if (cube[pos].has_value()) {
-                continue;
+        std::vector<std::size_t> dcPositions;
+        dcPositions.reserve(size());
+        for (auto pos = 0U; pos < size(); pos++) {
+            if (!cube[pos].has_value()) // if DC
+            {
+                dcPositions.emplace_back(pos);
             }
-            // recursively compute all the complete cubes for the zero case
-            Cube zero(cube);
-            zero[pos] = false;
-
-            auto completeZero = zero.completeCubes();
-            // move the computed cubes to the result vector
-            result.insert(result.end(),
-                          std::make_move_iterator(completeZero.begin()),
-                          std::make_move_iterator(completeZero.end()));
-
-            // recursively compute all the complete cubes for the one case
-
-            Cube one(cube);
-            one[pos] = true;
-
-            auto completeOne = one.completeCubes();
-            // move the computed cubes to the result vector
-            result.insert(result.end(),
-                          std::make_move_iterator(completeOne.begin()),
-                          std::make_move_iterator(completeOne.end()));
         }
-        // handle the case where the cube was already complete
-        if (result.empty()) {
-            result.emplace_back(cube);
+
+        Vector     result{};
+        const auto dcVecSize = dcPositions.size();
+        const auto dcSize    = 1U << dcVecSize;
+        result.reserve(dcSize);
+        Cube dcCube(cube);
+
+        for (auto i = 0U; i < dcSize; ++i) {
+            for (auto j = 0U; j < dcVecSize; ++j) {
+                const auto localBit = (i & (1U << (dcVecSize - j - 1))) != 0;
+
+                dcCube[dcPositions[j]] = localBit;
+            }
+            result.emplace_back(dcCube);
         }
         return result;
     }
